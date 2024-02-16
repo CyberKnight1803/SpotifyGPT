0a5e94d9107f305710538a03b1471437b68451dc
CyberKnight1803 <omkarpitale2001@gmail.com>
2024-02-15 23:13:21 -0500

    remove state check

--- src/spotify.py
+++ src/spotify.py
@@ -47,9 +47,6 @@ async def spotify_login(response: Response):
     """
         User Spotify login
     """
-    
-    # Generate random state 
-    state = generate_random_string(10)
 
     # Scope: Need access to create a new private playlist
     scope = "user-read-private user-read-email playlist-modify-private"
@@ -60,7 +57,6 @@ async def spotify_login(response: Response):
         "client_id": SPOTIFY_CLIENT_ID, 
         "scope": scope, 
         "redirect_uri": REDIRECT_URI, 
-        "state": state
     }
 
     # Redirect to spotify auth
@@ -68,8 +64,6 @@ async def spotify_login(response: Response):
         url=SPOTIFY_AUTH_URL + '?' + urlencode(params)
     )
 
-    # Set cookie to check if state value remains unchanged in callback
-    response.set_cookie(key=STATE_KEY, value=state)
     return response
 
 @router.get('/callback')
@@ -77,56 +71,47 @@ async def callback(request: Request, response: Response):
 
     # Access query params
     code = request.query_params["code"]
-    state = request.query_params["state"]
-    stored_state = request.cookies.get(STATE_KEY)
 
-    # If state value has changed, stop the flow and throw error
-    if state is None or state != stored_state:
-        raise HTTPException(status_code=400, detail="State mismatch")
-    
-    else:
-        # Delete coookie
-        response.delete_cookie(STATE_KEY, path="/", domain=None)
 
-        # base64 encode client_id and client_secret
-        request_string = SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET
-        encoded_bytes = base64.b64encode(request_string.encode("utf-8"))
-        encoded_string = str(encoded_bytes, "utf-8")
+    # base64 encode client_id and client_secret
+    request_string = SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET
+    encoded_bytes = base64.b64encode(request_string.encode("utf-8"))
+    encoded_string = str(encoded_bytes, "utf-8")
 
-        # Feed them as header
-        header = {"Authorization": "Basic " + encoded_string}
+    # Feed them as header
+    header = {"Authorization": "Basic " + encoded_string}
 
-        # Payload
-        form_data = {
-            "code": code,
-            "redirect_uri": REDIRECT_URI,
-            "grant_type": "authorization_code",
+    # Payload
+    form_data = {
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+        "grant_type": "authorization_code",
+    }
+
+    # Post request to get spotify api access token
+    api_response = requests.post(SPOTIFY_TOKEN_URL, data=form_data, headers=header)
+    if api_response.status_code == 200:
+        data = api_response.json()
+
+        # print(type(data))
+
+        url = SPOTIFY_BASE_URL + "/me"
+        headers = {
+            "Authorization": f"Authorization: Bearer {data['access_token']}" 
         }
 
-        # Post request to get spotify api access token
-        api_response = requests.post(SPOTIFY_TOKEN_URL, data=form_data, headers=header)
-        if api_response.status_code == 200:
-            data = api_response.json()
+        user_data_response = requests.get(url, headers=headers)
+        user_data = user_data_response.json()
 
-            # print(type(data))
+        data.update({'name': user_data['display_name']})
+        data.update({'email': user_data['email']})
+        data.update({'user_id': user_data['id']})
+        data.update({'user_profile': user_data['external_urls']['spotify']})
+        return data
 
-            url = SPOTIFY_BASE_URL + "/me"
-            headers = {
-                "Authorization": f"Authorization: Bearer {data['access_token']}" 
-            }
-
-            user_data_response = requests.get(url, headers=headers)
-            user_data = user_data_response.json()
-
-            data.update({'name': user_data['display_name']})
-            data.update({'email': user_data['email']})
-            data.update({'user_id': user_data['id']})
-            data.update({'user_profile': user_data['external_urls']['spotify']})
-            return data
-
-        else:
-            # State value is not same => Stop the process and throw error 
-            raise HTTPException(status_code=400, detail="Error in getting Spotify token")    
+    else:
+        # State value is not same => Stop the process and throw error 
+        raise HTTPException(status_code=400, detail="Error in getting Spotify token")    
 
 
 
