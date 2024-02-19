34d1d93c760abab2cf867b8c79a25bc379cc62af
CyberKnight1803 <omkarpitale2001@gmail.com>
2024-02-18 23:11:25 -0500

    add starlette CORS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from fastapi import FastAPI
 # from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 
@@ -13,20 +14,37 @@ from config import (
     RESPONSE_TIME_LIMIT, 
 )
 
+# CORS middleware
+
+origins = [
+    'http://localhost:3000', 
+    'http://0.0.0.0:3000', 
+    '*'
+]
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_methods=["*"], 
+        allow_headers=["*"], 
+        allow_credentials=True
+    )
+]
 
 # Create FastAPI app instance 
-app = FastAPI()
+app = FastAPI(middleware=middleware)
 
 # Cross-Origin Resource sharing
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_methods=["*"], 
-    allow_headers=["*"], 
-    allow_credentials=True, 
-    expose_headers=["Access-Control-Allow-Origin"],
-    max_age=RESPONSE_TIME_LIMIT
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"], 
+#     allow_methods=["*"], 
+#     allow_headers=["*"], 
+#     allow_credentials=True, 
+#     expose_headers=["Access-Control-Allow-Origin"],
+#     max_age=RESPONSE_TIME_LIMIT
+# )
 
 
 # Add Routers 
